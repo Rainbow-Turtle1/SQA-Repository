e2e59f8059ec118ebf5c16095f9d9c18906214ca
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2024-12-19 13:01:47 +0000

    feat/add-basic-register-functionality

--- models/user.js
+++ models/user.js
@@ -1,14 +1,24 @@
+<<<<<<< HEAD
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
+=======
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');  
+>>>>>>> 676e230 (feat/add-basic-register-functionality)
 
 const User = sequelize.define('User', {
   name: {
     type: DataTypes.STRING,
+<<<<<<< HEAD
     allowNull: false,
+=======
+    allowNull: false
+>>>>>>> 676e230 (feat/add-basic-register-functionality)
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
+<<<<<<< HEAD
     unique: true,
   },
   password: {
@@ -18,3 +28,14 @@ const User = sequelize.define('User', {
 });
 
 export default User;
+=======
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = { sequelize, User };
+>>>>>>> 676e230 (feat/add-basic-register-functionality)
