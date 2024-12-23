b27be93dab2085f2d61f92f6ccaee8afbeb070ff
Asher De Souza <77273625+asherddesouza@users.noreply.github.com>
2024-12-23 16:41:50 +0000

    adding UI for profile page

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 const router = Router();
-import { User } from '../models/user.js'
+// import { User } from '../models/user.js'
 
 router.get('/profile', (req, res) => {
-	res.render('register', { title: 'Register'})
+	res.render('profile', { title: 'Profile'})
 })
+
+export default router
