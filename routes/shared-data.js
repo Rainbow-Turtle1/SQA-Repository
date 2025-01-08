9cc23e5612a2c94f8d10d454ac396ec16aed543b
Asher De Souza <asher.desouza@clearscore.com>
2025-01-08 14:53:33 +0000

    added ability to change profile picture

---
+++ routes/shared-data.js
@@ -0,0 +1,11 @@
+// this file is temporary, and will be removed once we user authentication is implemented
+
+let accountProfilePicture = 0;
+
+export function getAccountProfilePicture() {
+  return accountProfilePicture;
+}
+
+export function setAccountProfilePicture(index) {
+  accountProfilePicture = index;
+}
