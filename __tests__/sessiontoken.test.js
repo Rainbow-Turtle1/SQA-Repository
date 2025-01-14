4ae61191094a0acbf7d7ddda127fb20ba103e803
Rainbow-Turtle1 <arran.j.mcp@gmail.com>
2025-01-14 11:42:30 +0000

    test fixes

--- __tests__/sessiontoken.test.js
+++ __tests__/sessiontoken.test.js
@@ -1,22 +1,22 @@
 import { NewSessionToken } from '../routes/sessionTokens.js';
 
 beforeEach(() => {
-    global.sessionStorage = {
-      getItem: jest.fn(),
-      setItem: jest.fn(),
-    };
+  globalThis.sessionStorage = {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+  };
 });
 
 test("NewSessionToken creates a token if none exists", () => {
-    sessionStorage.getItem.mockReturnValue(null); // Simulate no token in storage? HOPEFULLY??
+  sessionStorage.getItem.mockReturnValue(null); 
 
-    const userId = "user123"
-    const token = NewSessionToken(userId);
+  const userId = "user123";
+  const token = NewSessionToken(userId);
 
-    expect(sessionStorage.getItem).toHaveBeenCalledWith("sessionToken")
-    expect(sessionStorage.setItem).toHaveBeenCalledWith(
-        "sessionToken",
-        expect.any(String)
-      )   
-      expect(token).toBeDefined()
+  expect(sessionStorage.getItem).toHaveBeenCalledWith("sessionToken");
+  expect(sessionStorage.setItem).toHaveBeenCalledWith(
+    "sessionToken",
+    expect.any(String)
+  );
+  expect(token).toBeDefined();
 });
