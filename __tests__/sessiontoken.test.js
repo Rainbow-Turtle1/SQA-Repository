f6a39dd53880565fcf4ac903f3cba75c136c2864
Arran McPherson <arran.mcpherson@clearscore.com>
2025-01-30 17:09:32 +0000

    mockrequests and changes to use cookies

--- __tests__/sessiontoken.test.js
+++ __tests__/sessiontoken.test.js
@@ -1,5 +1,5 @@
 import "jest-localstorage-mock";
-import { afterAll, jest } from "@jest/globals";
+import { afterAll /*, jest */ } from "@jest/globals";
 import { NewSessionToken } from "../routes/session-tokens.js";
 import { sequelize } from "../models/user.js";
 
@@ -16,16 +16,29 @@ afterAll(async () => {
   await sequelize.close(); // Properly close database connection
 });
 
+function MockReq() {
+  return {
+    session: {},
+  };
+}
 test("NewSessionToken creates a token if none exists", () => {
+  const req = MockReq();
+  NewSessionToken(req, "id-for-tests");
+  expect(req.session.user).toEqual({
+    id: "id-for-tests",
+    date: expect.any(String),
+  });
+
+  //depricated
   //sessionStorage.getItem.mockReturnValue(null);
 
-  const userId = "user123";
-  const token = NewSessionToken(userId);
+  // const userId = "user123";
+  // const token = NewSessionToken(userId);
 
   // expect(sessionStorage.getItem).toHaveBeenCalledWith("sessionToken");
-  expect(sessionStorage.setItem).toHaveBeenCalledWith(
-    "sessionToken",
-    expect.any(String)
-  );
+  // expect(sessionStorage.setItem).toHaveBeenCalledWith(
+  // "sessionToken",
+  // expect.any(String)
+  // );
   // expect(token).toBeDefined();
 });
