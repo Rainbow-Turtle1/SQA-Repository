590f34aa41788318fd8c6680a9330196de1f1a61
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2025-02-04 12:32:21 +0000

    Add SQL injection protection and input v alidation

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,4 +1,4 @@
-import { jest } from "@jest/globals";
+import { describe, it, jest } from "@jest/globals";
 import express from "express";
 import bcrypt from "bcryptjs";
 import { sequelize, User } from "../models/user.js";
@@ -82,8 +82,8 @@ describe("POST /register happy cases", () => {
     const req = mockRequest({
       name: "Isabella",
       email: "Isabella@email.com",
-      password: "1234",
-      confirmPassword: "1234",
+      password: "123456",
+      confirmPassword: "123456",
     });
     const res = mockResponse();
 
@@ -95,8 +95,61 @@ describe("POST /register happy cases", () => {
     });
     expect(user).not.toBeNull();
     expect(user.uuid).not.toBeNull();
-    expect(await bcrypt.compare("1234", user.password)).toBe(true);
+    expect(await bcrypt.compare("123456", user.password)).toBe(true);
   });
+
+  it("should redirect the user to login if they are already registered", async () => {
+    await User.create({
+      name: "Isabella",
+      email: "isabella@email.com",
+      password: await bcrypt.hash("123456", 10),
+    });
+  
+    const req = mockRequest({
+      name: "Isabella",
+      email: "isabella@email.com",
+      password: "123456",
+      confirmPassword: "123456",
+    });
+    const res = mockResponse();
+  
+    await UserRoutes.stack
+      .find((r) => r.route.path === "/register" && r.route.methods.post)
+      .route.stack[0].handle(req, res);
+  
+    const user = await User.findOne({ where: { email: "isabella@email.com" } });
+  
+    expect(user).not.toBeNull();
+    expect(user.id).not.toBeNull(); 
+    expect(await bcrypt.compare("123456", user.password)).toBe(true);
+  
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        success: false,
+        message: "This email is already registered. Redirecting to login...",
+        redirectUrl: "/login",
+      })
+    );
+  });
+
+  //     name: "Isabella",
+  //     email: "Isabella@email.com",
+  //     password: "123456",
+  //     confirmPassword: "123456",
+  //   });
+  //   const res = mockResponse();
+
+  //   await UserRoutes.stack
+  //     .find((r) => r.route.path === "/register" && r.route.methods.post)
+  //     .route.stack[0].handle(req, res);
+  //   const user = await User.findOne({
+  //     where: { email: "Isabella@email.com" },
+  //   });
+  //   expect(user).not.toBeNull();
+  //   expect(user.uuid).not.toBeNull();
+  //   expect(await bcrypt.compare("123456", user.password)).toBe(true);
+  // });
 });
 
 describe("POST /register error cases", () => {
@@ -126,7 +179,7 @@ describe("POST /register error cases", () => {
     const req = mockRequest({
       name: "Isabella",
       email: "Isabella@email.com",
-      password: "1234",
+      password: "123456",
       confirmPassword: "5678",
     });
     const res = mockResponse();
@@ -141,6 +194,47 @@ describe("POST /register error cases", () => {
       message: "Your passwords do not match. Please retry.",
     });
   });
+
+  it("should return an error if password is less than 6 characters", async () => {
+    const req = mockRequest({
+      name: "Isabella",
+      email: "Isabella@email.com",
+      password: "1234",
+      confirmPassword: "5678",
+    });
+    const res = mockResponse();
+
+    await UserRoutes.stack
+      .find((r) => r.route.path === "/register" && r.route.methods.post)
+      .route.stack[0].handle(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      success: false,
+      message: "Your password must be at least 6 characters long.",
+    });
+  });
+
+  it("should return an error if email is in an invalid format.", async () => {
+    const req = mockRequest({
+      name: "Isabella",
+      email: "isabellaemail.com",
+      password: "123456",
+      confirmPassword: "5678",
+    });
+    const res = mockResponse();
+
+    await UserRoutes.stack
+      .find((r) => r.route.path === "/register" && r.route.methods.post)
+      .route.stack[0].handle(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      success: false,
+      message: "Your email is in an invalid format.",
+    });
+  });
+  
 });
 
 describe("POST /login happy paths", () => {
@@ -187,7 +281,7 @@ describe("POST /login error cases", () => {
     expect(res.status).toHaveBeenCalledWith(400);
     expect(res.json).toHaveBeenCalledWith({
       success: false,
-      message: "All fields are required to log in!",
+      message: "All fields are required to log in.",
     });
   });
 
@@ -209,6 +303,7 @@ describe("POST /login error cases", () => {
       success: false,
     });
   });
+
   it("should return an error if the password is incorrect", async () => {
     const password = await bcrypt.hash("securepassword", 10);
     const uuid = uuidv4();
@@ -233,6 +328,33 @@ describe("POST /login error cases", () => {
     expect(res.json).toHaveBeenCalledWith({
       success: false,
       message: "Incorrect password. Please try again.",
+    });
+  });
+
+  it("should return an error if the email is in an invalid format.", async () => {
+    const password = await bcrypt.hash("securepassword", 10);
+    const uuid = uuidv4();
+    await User.create({
+      uuid: uuid,
+      name: "Isabella",
+      email: "isabellainvalid.com",
+      password,
+    });
+
+    const req = mockRequest({
+      email: "isabellainvalid.com",
+      password: "apples",
+    });
+    const res = mockResponse();
+
+    await UserRoutes.stack
+      .find((r) => r.route.path === "/login" && r.route.methods.post)
+      .route.stack[0].handle(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      success: false,
+      message: "Your email is in an invalid format.",
     });
   });
 
@@ -261,5 +383,35 @@ describe("POST /login error cases", () => {
       expect.any(Error)
     );
     spy.mockRestore();
+  });
+});
+
+//this test has to be run last because it is affecting other tests
+describe("POST /register error case", () => {
+  it("should return an error if there is a server error", async () => {
+
+    jest.spyOn(User, "create").mockRejectedValue(new Error("Database error"));
+  
+    const req = mockRequest({
+      name: "Isabella",
+      email: "isabella@email.com",
+      password: "123456",
+      confirmPassword: "123456",
+    });
+    const res = mockResponse();
+  
+    await UserRoutes.stack
+      .find((r) => r.route.path === "/register" && r.route.methods.post)
+      .route.stack[0].handle(req, res);
+  
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        success: false,
+        message: "An error occurred. Please try again.",
+      })
+    );
+  
+    User.create.mockRestore();
   });
 });
