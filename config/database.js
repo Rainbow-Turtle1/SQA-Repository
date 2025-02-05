bac2220dfe340733f63c6e997ab156adf6a6fea1
Nathan Voong <nathan.voong@clearscore.com>
2025-02-05 11:31:37 +0000

    remove edit button if user cannot edit

--- config/database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-import { join } from "path";
-
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: join(new URL('.', import.meta.url).pathname, "..", "database.sqlite"),
-});
-
-export default sequelize;
